5f18657c307cac527e2104917e5e9924edf50282
Matheus Guanaz <matheusguanaz@hotmail.com>
2022-03-01 15:40:06 -0300

    first commit

---
+++ backend/database/db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE movies (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	type INT NOT NULL,
+	name VARCHAR(30) NOT NULL,
+	total_ep INT,
+	atual_ep INT,
+	last_view DATE DEFAULT CURRENT_TIMESTAMP
+);
+
